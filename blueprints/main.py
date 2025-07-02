0cf32c6203d72b1064c8dd6c63e8f63a9b90340d
AmirHossein Naei <info@amirhn.ir>
2025-07-02 18:37:29 +0300

    count view while redirecting

--- blueprints/main.py
+++ blueprints/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from ext import db
 from models import Link
 
 app = Blueprint('main', __name__)
@@ -8,4 +9,6 @@ app = Blueprint('main', __name__)
 @app.route('/<short_id>')
 def redirect(short_id):
     link = Link.query.filter_by(short_id=short_id).first_or_404()
+    link.views += 1
+    db.session.commit()
     return render_template('main/redirect.html', long_url=link.long_url)
