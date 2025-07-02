8c35c476c3eeedfcba05297cda2e27c566c942ef
AmirHossein Naei <info@amirhn.ir>
2025-07-02 18:33:04 +0300

    adding main bp and redirect route

---
+++ blueprints/main.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+from models import Link
+
+app = Blueprint('main', __name__)
+
+
+@app.route('/<short_id>')
+def redirect(short_id):
+    link = Link.query.filter_by(short_id=short_id).first_or_404()
+    return render_template('main/redirect.html', long_url=link.long_url)
