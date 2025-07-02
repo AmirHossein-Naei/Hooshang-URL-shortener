339e08268cfdb10d0f72a1407e257054b874ea0e
AmirHossein Naei <info@amirhn.ir>
2025-07-02 18:25:00 +0300

    change models.py for adding user_id and  view to link

--- models.py
+++ models.py
@@ -23,8 +23,11 @@ class Link(db.Model):
     id = Column(Integer, primary_key=True)
     short_id = Column(String, nullable=False, index=True)
     long_url = Column(String, nullable=False)
-
+    views = Column(Integer, default=0)
     created_at = Column(default=get_current_timestamp)
+
+    user_id = Column(Integer, db.ForeignKey(User.id), nullable=False)
+    user = db.relationship(User, backref=db.backref('links', lazy=True))
 
     def __repr__(self):
         return f'<Link {self.id}>'
