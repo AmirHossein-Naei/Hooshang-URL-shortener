7b1c7948a38054616f3787509292221e4eabd292
AmirHossein Naei <info@amirhn.ir>
2025-07-23 17:30:03 +0300

    better default secretkey in config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SECRET_KEY = 'ljd829SSS4372398h###sdasdasudh%$^$123123asuidahsd'
+SECRET_KEY = 'SECRET_KEY'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 
 ADMIN_REGISTRATION_IS_ALLOWED = False
