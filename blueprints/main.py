627f051c1a3c9da384efb17b26ef42526eff5ec4
AmirHossein Naei <info@amirhn.ir>
2025-08-06 17:06:26 +0300

    set redirect url for index page

--- blueprints/main.py
+++ blueprints/main.py
@@ -1,13 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort, redirect
 
+import config
 from ext import db
 from models import Link
 
 app = Blueprint('main', __name__)
 
 
+@app.route('/')
+def index():
+    if config.MAIN_PAGE_REDIRECT_TO == "":
+        abort(404)
+    else:
+        return redirect(config.MAIN_PAGE_REDIRECT_TO)
+
+
 @app.route('/<short_id>')
-def redirect(short_id):
+def redirect_to_long_url(short_id):
     short_id = short_id.lower()
 
     link = Link.query.filter_by(short_id=short_id).first_or_404()
